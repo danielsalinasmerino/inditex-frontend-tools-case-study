dd0103e68c3c8a0b1bbc9dfa7310d1e408192643
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-02-18 10:03:48 +0100

    feat: Cleaned files, added CSS reset

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// TODO: Rework this once we know how the screen will be
+test.skip("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
