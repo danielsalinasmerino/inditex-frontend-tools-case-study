df3c58135b2d986b6957ca55e8dda0fd02d492bc
danielsalinasmerino <danielsalinas305@gmail.com>
2023-02-19 12:18:51 +0100

    feat(RowElement): RowElement now takes into account the template info

--- src/components/molecules/RowElement/RowElement.tsx
+++ src/components/molecules/RowElement/RowElement.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { createProductsFixture } from "../../../fixtures/products";
+import { createTemplateFixture } from "../../../fixtures/templates";
 import { Row } from "../../../models/rows";
 import { ProductCard } from "../../atoms/ProductCard";
 import "./RowElement.css";
@@ -9,20 +10,29 @@ export type RowElementProps = {
 };
 
 export const RowElement: FC<RowElementProps> = ({ row }) => {
-  const { id, productIds, templateId, position } = row;
+  const { productIds, templateId } = row;
+
+  const products = createProductsFixture();
+
+  const { name: templateName, alignment: templateAlignment } =
+    createTemplateFixture();
+
+  const contentStyle: React.CSSProperties = {
+    justifyContent: templateAlignment,
+  };
 
   return (
     <div className="rowElement">
       <div className="header">
-        <div className="title">Hola</div>
+        <div className="title">{templateName}</div>
         <div className="buttonsContainer">
           <span>Cambiar alineado</span>
           <span>Mover</span>
           <span>Borrar</span>
         </div>
       </div>
-      <div className="content" style={{ justifyContent: "right" }}>
-        {createProductsFixture().map((product) => {
+      <div className="content" style={contentStyle}>
+        {products.map((product) => {
           return (
             <ProductCard key={`product-card-${product.id}`} product={product} />
           );
