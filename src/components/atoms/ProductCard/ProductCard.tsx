d27a65bfc202dbefb5f34991026bdad42adf7796
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-02-20 17:08:36 +0100

    feat: ProductCard visual improvements

--- src/components/atoms/ProductCard/ProductCard.tsx
+++ src/components/atoms/ProductCard/ProductCard.tsx
@@ -13,17 +13,14 @@ const ALT_PRODUCT_CARD_IMAGE_TEXT = "Product Image";
 export const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { id, name, image, price, currency } = product;
 
-  const productInfo = `${name} ${formatPriceToPriceWithCurrency(
-    price,
-    currency
-  )}`;
-
   return (
     <Draggable draggableId={id}>
       <div className="productCard">
         <img src={image} alt={ALT_PRODUCT_CARD_IMAGE_TEXT} />
         <div className="productCardInfo">
-          <span>{productInfo}</span>
+          <span>{name}</span>
+          <br />
+          <span>{formatPriceToPriceWithCurrency(price, currency)}</span>
         </div>
       </div>
     </Draggable>
