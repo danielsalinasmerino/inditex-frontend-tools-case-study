2356f36438e73f3a39c20c89f6d530b8f03a6278
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-02-21 16:17:55 +0100

    feat(ProductCard.spec): Added tests

--- src/components/atoms/ProductCard/ProductCard.spec.tsx
+++ src/components/atoms/ProductCard/ProductCard.spec.tsx
@@ -1,2 +1,19 @@
-// TODO: Rework this
-export {};
+import { createProductFixture } from "../../../fixtures/products";
+import { render, screen } from "../../../tests/appTestUtils";
+import { formatPriceToPriceWithCurrency } from "../../../utils/currencies";
+import { ProductCard } from "./ProductCard";
+
+const PRODUCT = createProductFixture();
+
+describe("ProductCard", () => {
+  it("renders product information correctly", () => {
+    render(<ProductCard product={PRODUCT} />);
+
+    const { name, price, currency } = PRODUCT;
+
+    expect(screen.getByText(name)).toBeInTheDocument();
+    expect(
+      screen.getByText(formatPriceToPriceWithCurrency(price, currency))
+    ).toBeInTheDocument();
+  });
+});
