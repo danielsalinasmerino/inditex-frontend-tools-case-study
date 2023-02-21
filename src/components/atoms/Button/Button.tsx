5c028d6f36838384ae0cad4a033ebe3bb8f587c0
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-02-21 12:03:40 +0100

    feat(Button): Added styling for disabled button

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -5,15 +5,22 @@ export type ButtonProps = {
   label: string;
   onClick: () => void;
   style?: React.CSSProperties;
+  disabled?: boolean;
 };
 
-export const Button: FC<ButtonProps> = ({ label, onClick, style }) => {
+export const Button: FC<ButtonProps> = ({
+  label,
+  onClick,
+  style,
+  disabled,
+}) => {
   return (
     <button
       type="button"
       onMouseDown={onClick}
       className="button"
       style={style}
+      disabled={disabled}
     >
       {label}
     </button>
