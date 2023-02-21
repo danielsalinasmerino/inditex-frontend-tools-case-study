5463476f62920e9d6a72bec1dae59821d7a5e633
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-02-21 11:06:25 +0100

    feat: Now we can zoom in and zoom out the gird

--- src/components/pages/CaseStudy/CaseStudy.tsx
+++ src/components/pages/CaseStudy/CaseStudy.tsx
@@ -4,6 +4,7 @@ import { useFetchProducts } from "../../../repositories/products/ProductsReposit
 import { createGridFromProducts } from "../../../utils/grid";
 import { GridElement } from "../../organisms/GridElement";
 import ZARA_LOGO from "../../../assets/png/logo.png";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import "./CaseStudy.css";
 
 export type CaseStudyProps = {};
@@ -22,13 +23,39 @@ export const CaseStudy: FC<CaseStudyProps> = () => {
     }
   }, [products]);
 
+  const transformComponentStyles: React.CSSProperties = {
+    width: "100%",
+    height: "100%",
+  };
+
   if (isLoadingProducts) return null;
 
   return (
     <div className="container">
       <img className="logo" src={ZARA_LOGO} alt={ZARA_LOGO_ALT_TEXT} />
+      <div>Parrilla de productos</div>
       {grid && (
-        <GridElement grid={grid} products={products} onGridChange={setGrid} />
+        <TransformWrapper
+          initialScale={1}
+          minScale={0.3}
+          maxScale={1}
+          wheel={{
+            step: 0.02,
+            activationKeys: ["Meta", "Control"],
+          }}
+          panning={{ disabled: true }}
+        >
+          <TransformComponent
+            wrapperStyle={transformComponentStyles}
+            contentStyle={transformComponentStyles}
+          >
+            <GridElement
+              grid={grid}
+              products={products}
+              onGridChange={setGrid}
+            />
+          </TransformComponent>
+        </TransformWrapper>
       )}
     </div>
   );
